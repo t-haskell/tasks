57446349f76846c2749ec62f078d308ad5350cf8
t-haskell <tommyhaskell3@gmail.com>
2023-02-11 16:10:43 -0500

    Adding Hello World.

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Website by Tommy Haskell. I love CISC275.
+                automatically reload. Website by Tommy Haskell. Hello World.
             </p>
         </div>
     );
