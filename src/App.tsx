17d892c7cb63f6ca4a94f9944fd4ffe16cbe29d9
Tommy <tommyhaskell3@gmail.com>
2023-02-15 19:14:04 -0500

    Adding 2 Column Table and Fixing Image

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 import dog from "./my_dog.jpeg";
 
@@ -14,7 +14,16 @@ function App(): JSX.Element {
                 automatically reload. Website by Tommy Haskell. Hello World.
             </p>
             <h1 className="App-myHeader">Welcome to Tommy&#39;s Website</h1>
-            <img src={dog} alt="A picture of my dog Niko" />
+            <img
+                src={dog}
+                alt="A picture of my dog Niko"
+                style={{
+                    maxWidth: "100%",
+                    maxHeight: "100%",
+                    display: "block",
+                    margin: "auto"
+                }}
+            />
             Sports Ive Played:
             <ul>
                 <li>Lacrosse</li>
@@ -24,6 +33,30 @@ function App(): JSX.Element {
             <Button onClick={() => console.log("Hello World!")}>
                 Log Hello World
             </Button>
+            <div>
+                <Container>
+                    <Row>
+                        <Col>
+                            <div
+                                style={{
+                                    backgroundColor: "red",
+                                    width: "100px",
+                                    height: "500px"
+                                }}
+                            ></div>
+                        </Col>
+                        <Col>
+                            <div
+                                style={{
+                                    backgroundColor: "red",
+                                    width: "100px",
+                                    height: "500px"
+                                }}
+                            ></div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 }
