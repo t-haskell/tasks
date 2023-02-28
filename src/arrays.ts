759df8debc73e616382944c4efa4cd12176a4292
Tommy <tommyhaskell3@gmail.com>
2023-02-27 20:52:09 -0500

    First attempt at makeMath

--- src/arrays.ts
+++ src/arrays.ts
@@ -107,7 +107,16 @@ export function allRGB(colors: string[]): boolean {
  * And the array [] would become "0=0".
  */
 export function makeMath(addends: number[]): string {
-    return "";
+    if (addends.length === 0) {
+        return "0=0";
+    }
+    const equation = addends.toString();
+    const total = addends.reduce(
+        (currTotal: number, num: number) => currTotal + num,
+        0
+    );
+    const insertingPlus = equation.replaceAll(",","+");
+    return total + "=" + insertingPlus;
 }
 
 /**
