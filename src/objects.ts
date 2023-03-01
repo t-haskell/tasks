4de79aea0209048c9460ffde436c83b869e033d2
Tommy <tommyhaskell3@gmail.com>
2023-02-28 21:05:37 -0500

    First attempt on isValid

--- src/objects.ts
+++ src/objects.ts
@@ -43,7 +43,13 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    if (question.type === "short_answer_question") {
+        return true;
+    } else {
+        return question.options.some(
+            (option: string): boolean => answer === option
+        );
+    }
 }
 
 /**
