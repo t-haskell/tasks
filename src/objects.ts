438e35da5a8317fbd69315b2f82cfa4085fc1517
Tommy <tommyhaskell3@gmail.com>
2023-03-01 23:13:59 -0500

    First attempt at duplicateQuestion

--- src/objects.ts
+++ src/objects.ts
@@ -128,7 +128,11 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return oldQuestion;
+    return {
+        ...oldQuestion,
+        name: "Copy of " + oldQuestion.name,
+        published: false
+    };
 }
 
 /**
