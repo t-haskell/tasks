d033e0b05865045600c9bc8be3bfe5ddcf18611d
Tommy <tommyhaskell3@gmail.com>
2023-03-06 19:05:21 -0500

    Completed with shallow copy protocols

--- src/nested.ts
+++ src/nested.ts
@@ -1,6 +1,7 @@
 import { table } from "console";
 import { Answer } from "./interfaces/answer";
 import { Question, QuestionType } from "./interfaces/question";
+import { makeBlankQuestion } from "./objects";
 
 /**
  * Consumes an array of questions and returns a new array with only the questions
@@ -180,7 +181,8 @@ export function addNewQuestion(
     name: string,
     type: QuestionType
 ): Question[] {
-    return [];
+    const newQuestions = [...questions, makeBlankQuestion(id, name, type)];
+    return newQuestions;
 }
 
 /***
