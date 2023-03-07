8735000024b9ccf6727792cb9d20c587780328cf
Tommy <tommyhaskell3@gmail.com>
2023-03-07 15:14:13 -0500

    Finished ChangeType with ternary operations

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,22 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [currType, changeType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+
+    return (
+        <div>
+            <Button
+                onClick={() =>
+                    currType === "multiple_choice_question"
+                        ? changeType("short_answer_question")
+                        : changeType("multiple_choice_question")
+                }
+            >
+                Change Type
+            </Button>
+            {currType}
+        </div>
+    );
 }
