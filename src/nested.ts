080152d8f2d15753261222957ce4572c4873c28a
Tommy <tommyhaskell3@gmail.com>
2023-03-06 20:35:50 -0500

    Had to disable es-lint for importing table

--- src/nested.ts
+++ src/nested.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { table } from "console";
 import { Answer } from "./interfaces/answer";
 import { Question, QuestionType } from "./interfaces/question";
