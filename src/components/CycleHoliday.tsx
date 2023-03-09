fb8af9a9724fab2f53e2237183fc7183a60acbad
Tommy <tommyhaskell3@gmail.com>
2023-03-09 14:22:32 -0500

    Added buttons into CycleHoliday and second record

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -15,8 +15,33 @@ const holidaysAlphabetically: Record<Holiday, Holiday> = {
     Memorial_Day: "New_Years_Eve",
     New_Years_Eve: "Christmas"
 };
+const holidaysByDate: Record<Holiday, Holiday> = {
+    Easter: "Memorial_Day",
+    Memorial_Day: "Halloween",
+    Halloween: "Christmas",
+    Christmas: "New_Years_Eve",
+    New_Years_Eve: "Easter"
+};
+
 export function CycleHoliday(): JSX.Element {
     const [currHoliday, setHoliday] = useState<Holiday>("Easter");
 
-    return <div>Cycle Holiday</div>;
+    return (
+        <div>
+            Cycle Holiday
+            <br />
+            Current Holiday: {currHoliday}
+            <Button
+                onClick={() => setHoliday(holidaysAlphabetically[currHoliday])}
+            >
+                Advance by Alphabet
+            </Button>
+            <Button
+                onClick={() => setHoliday(holidaysAlphabetically[currHoliday])}
+            >
+                Advance by Year
+            </Button>
+            Cycle Holiday
+        </div>
+    );
 }
