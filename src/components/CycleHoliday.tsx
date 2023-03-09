f957e36c9f6aa03ad57868a2b7375ef1cc876144
Tommy <tommyhaskell3@gmail.com>
2023-03-09 13:48:18 -0500

    Added first record to CycleHoliday

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,22 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+export type Holiday =
+    | "Christmas"
+    | "Easter"
+    | "New_Years_Eve"
+    | "Memorial_Day"
+    | "Halloween";
+
+const holidaysAlphabetically: Record<Holiday, Holiday> = {
+    Christmas: "Easter",
+    Easter: "Halloween",
+    Halloween: "Memorial_Day",
+    Memorial_Day: "New_Years_Eve",
+    New_Years_Eve: "Christmas"
+};
 export function CycleHoliday(): JSX.Element {
+    const [currHoliday, setHoliday] = useState<Holiday>("Easter");
+
     return <div>Cycle Holiday</div>;
 }
