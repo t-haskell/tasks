041d1e1df374ec12fb11083777909199aa009f34
Tommy <tommyhaskell3@gmail.com>
2023-03-12 14:16:05 -0400

    Commented out dhValue import

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import { DoubleHalf } from "./bad-components/DoubleHalf";
 import { ColoredBox } from "./bad-components/ColoredBox";
 import { ShoveBox } from "./bad-components/ShoveBox";
 import { ChooseTeam } from "./bad-components/ChooseTeam";
-import { dhValue } from "./bad-components/DoubleHalfState";
+//import { dhValue } from "./bad-components/DoubleHalfState";
 
 function App(): JSX.Element {
     return (
