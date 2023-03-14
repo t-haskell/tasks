598f85bcaa6d127bd56d0d6a04ef67dd65424a02
Tommy <tommyhaskell3@gmail.com>
2023-03-14 14:58:08 -0400

    Had to import react bootstrap

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [givenAnswer, setGiven] = useState<string>("");
+
+    function updateGiven(event: React.ChangeEvent<HTMLInputElement>) {
+        setGiven(event.target.value);
+    }
     return (
         <div>
             <h3>Check Answer</h3>
+            <div>
+                <Form.Group controlId="answer-guess">
+                    <Form.Label>Take a guess:</Form.Label>
+                    <Form.Control value={givenAnswer} onChange={updateGiven} />
+                </Form.Group>
+            </div>
         </div>
     );
 }
