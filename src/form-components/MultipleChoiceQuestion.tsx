99e31715249594657dc46dec4a52d2bdd7e0e7b4
Tommy <tommyhaskell3@gmail.com>
2023-03-20 10:38:38 -0400

    First selection dropdown attempt, passing all test

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,32 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    //State
+    const [currChoice, setChoice] = useState<string>(options[0]);
+
+    //Control
+    function updateChoice(event: React.ChangeEvent<HTMLSelectElement>) {
+        setChoice(event.target.value);
+    }
+
     return (
+        //View
         <div>
             <h3>Multiple Choice Question</h3>
+            <div>
+                <Form.Group controlId="dropdown-question">
+                    <Form.Label>
+                        {currChoice === expectedAnswer ? "✔️" : "❌"}
+                    </Form.Label>
+                    <Form.Select value={currChoice} onChange={updateChoice}>
+                        {options.map((option: string) => (
+                            <option key={option} value={option}>
+                                {option}
+                            </option>
+                        ))}
+                    </Form.Select>
+                </Form.Group>
+            </div>
         </div>
     );
 }
